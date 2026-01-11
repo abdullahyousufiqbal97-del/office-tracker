c6bf7275e3391ce634aa3953b01a093a7ac8b950
abdullahyousufiqbal97-del <abdullahyousufiqbal97@gmail.com>
2026-01-11 00:40:25 +0000

    Revise offline cache version and assets list
    Updated cache version and added sw.js to assets.

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
-// Minimal offline cache
-const CACHE = "office50-v1";
-const ASSETS = ["./", "./index.html", "./app.js", "./manifest.webmanifest"];
+// Offline cache (bump version when you deploy updates)
+const CACHE = "office50-v5";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.webmanifest", "./sw.js"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((c) => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))));
+    await self.clients.claim();
+  })());
 });
 
 self.addEventListener("fetch", (event) => {
